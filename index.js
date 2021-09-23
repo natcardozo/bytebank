b341ddc73137be069b1fdbd1cda9634147a3727c
Nathalia Cardozo <nathaliacardozosilva@gmail.com>
2021-09-22 23:15:40 -0300

    Curso 2 Bytebank

---
+++ index.js
@@ -0,0 +1,21 @@
+import { ContaCorrente } from "./ContaCorrente";
+import { Cliente } from "./Cliente";
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233344;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 55566677788;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+const contaCorrenteAlice = new ContaCorrente()
+contaCorrenteAlice.agencia = 1001;
+contaCorrenteAlice.saldo = 0;
